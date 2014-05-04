a315a675f430e1bf527a4c21146ac8884d4decb0
Sudhakar <sudhakar82@gmail.com>
2014-05-04 23:26:01 +0530

    Updated

---
+++ visualizations/budget/data.js
@@ -0,0 +1,105 @@
+budget2011_2012 = {
+    name: "2011-2012",
+    root: true,
+    children: [
+        {
+            name: "education",
+            children: [
+                {name: "national", size: 31036},
+                {name: "State level additional", size: 3675}
+            ]
+        },
+        {
+            name: "health",
+            children: [
+                {name: "NRHM", size: 22300}
+            ]
+        },
+        {
+            name: "infrastructure",
+            children: [
+                {name: "Roads", size: 19894},
+                {name: "Railways", size: 16752}
+            ]
+        },
+        {
+            name: "Rural Development",
+            children: [
+                {name: "Catch all", size: 66100},
+                {name: "Mahatma Gandhi National Rural Employment Guarantee Scheme", size: 40100},
+                {name: "Bharat Nirman", size: 48000},
+                {name: "Backward region grant fund", size: 7300},
+                {name: "Bundelkhand drought migitation", size: 1200}
+            ]
+        },
+        {
+            name: "defense",
+            children: [
+                {name: "Defensive Services", size: 147344}
+            ]
+        }
+    ]
+};
+
+budget2012_2013 = {
+    name: "2012-2013",
+    root: true,
+    children: [
+        {
+            name: "education",
+            children: [
+                {name: "rte ssa", size: 25555},
+                {name: "Rashtriya Madhyamik Shiksha Abhiyan", size: 3124},
+                {name: "Mid Day Meals", size: 11937},
+                {name: "Integrated Child Development Service", size: 15850},
+                {name: "Rajiv Gandhi Scheme for Empowerment of Adolescent Girls", size: 750}
+            ]
+        },
+        {
+            name: "health",
+            children: [
+                {name: "NRHM", size: 20822}
+            ]
+        },
+        {
+            name: "infrastructure",
+            children: [
+                {name: "Roads", size: 25360}
+            ]
+        },
+        {
+            name: "Rural Development",
+            children: [
+                {name: "National Rural Livelihood Mission", size: 3915},
+                {name: "Textiles", size: 3884},
+                {name: "Geo-Textiles", size: 500},
+                {name: "Powerloom cluster", size: 70},
+                {name: "Backward Regions Grant Fund scheme", size: 12040},
+                {name: "Prime minister employment generation program", size: 1276},
+                {name: "Rashtriya Krishi Vikas Yojana", size: 9217},
+                {name: "Bringing Green Revolution to Eastern India", size: 1000},
+                {name: "Vidarbha Intensified Irrigation Development Programme", size: 300},
+                {name: "Dairy Sector", size: 2242},
+                {name: "Costal aquaculture", size: 500},
+                {name: "Agriculture researh", size: 200},
+                {name: "AIBP", size: 14242},
+                {name: "Flood management project in Murshidabad", size: 439},
+                {name: "Scheduled Castes Sub Plan Allocation", size: 37113},
+                {name: "Tribal Sub Plan Allocation", size: 21710},
+                {name: "Rural drinking water and sanitation", size: 14000},
+                {name: "PMGSY", size: 24000},
+                {name: "Rural Infrastructure Development Fund", size: 20000}
+            ]
+        },
+        {
+            name: "defense",
+            children: [
+                {name: "Defensive Services", size: 193407},
+                {name: "Residetial Quarters", size: 1185},
+                {name: "Office building for central armed police forces", size: 3280}
+            ]
+        }
+    ]
+};
+
+dataSets = [budget2011_2012, budget2012_2013];
---
+++ visualizations/budget/script.js
@@ -0,0 +1,153 @@
+var svg = d3.select('svg');
+var groupColors = d3.scale.category10();
+
+//Switcher viz
+var switchers = svg.selectAll('.switcher')
+    .data(dataSets)
+    .enter()
+    .append("g")
+    .attr("class", "switcher");
+
+switchers.attr("transform", function (d, i) {
+    return "translate(" + (i * 70 + 50) + ",40)";
+})
+switchers.append("ellipse")
+    .attr("rx", 40)
+    .attr("ry", 30);
+switchers.append("text")
+    .style("text-anchor", "middle")
+    .attr("dy", ".3em")
+    .text(function (d) {
+        return d.name
+    });
+switchers.on("click", function (d) {
+    d3.select('.switcher.selected').classed('selected', false);
+    d3.select(this).classed('selected', true);
+    reload(d);
+});
+
+
+//Data viz
+var dim = 650;
+var offset = [100, 120];
+var pack = d3.layout.pack().size([dim, dim]).value(function (d) {
+    return d.size
+});
+var format = d3.format(",d");
+
+var group = svg.append("g");
+group.attr("transform", "translate(" + offset[0] + "," + offset[1] + ")");
+group.append('text')
+    .attr("class","title")
+    .attr("x", function() {return 280})
+    .attr("y", -10);
+
+function reload(data) {
+    group.select('.title')
+        .text(function() {return data.name});
+
+    var node = group.selectAll('.node')
+        .data(pack.nodes(data), function (d) {
+            return d['name']
+        });
+    var newNodes = node.enter()
+        .append("g")
+        .attr("class", function (d) {
+            return d.root ? "root node" : (d.children ? "trunk node" : "leaf node");
+        })
+        .attr("transform", function (d) {
+            return "translate(325,325)";
+        });
+    newNodes.append("circle");
+    newNodes.filter(function (d) {
+        return !d.children;
+    }).append("text");
+
+    var exitNodes = node.exit();
+    exitNodes.select('text').remove();
+    exitNodes.select('circle')
+        .transition()
+        .attr("r", function (d) {
+            return 0
+        })
+        .duration(500)
+    exitNodes
+        .transition().delay(500).remove();
+
+    node
+        .transition()
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        })
+        .duration(2000);
+
+    node.select("circle")
+        .transition()
+        .delay(1000)
+        .attr("r", function (d) {
+            return d.r;
+        })
+        .duration(1000);
+
+    node.filter(function (d) {
+        return !d.children;
+    }).select("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .transition()
+        .text(function (d) {
+            return d.name.substring(0, d.r / 3);
+        })
+        .delay(2000);
+    node.on("mouseover", showTooltip);
+    node.on("mouseout", hideTooltip);
+
+    group.selectAll('.trunk circle').style("fill", function (d, i) {
+        return groupColors(i)
+    });
+}
+
+function showTooltip(event) {
+    var size = [300, 100];
+    var data = d3.select(this).data()[0];
+    var tooltip = svg.append("g").attr("class", "tooltip");
+    var pointerSize = 8;
+
+    tooltip.attr("transform", "translate(" + (data.x + offset[0] - size[0] / 2) + "," + (data.y + offset[1] - data.r - size[1] - pointerSize) + ")");
+
+    tooltip.append("rect")
+        .attr("width", size[0]).attr("height", size[1])
+        .attr("rx", 4);
+    var pointer = [(data.x + offset[0]), (data.y + offset[1] - data.r)];
+
+    var lowerTriangle = [
+        [size[0] / 2, size[1] + pointerSize].join(","),
+        [size[0] / 2 - pointerSize, size[1]].join(","),
+        [size[0] / 2 + pointerSize, size[1]].join(",")].join(" ");
+    tooltip.append("polygon")
+        .attr("points", lowerTriangle);
+
+    tooltip.append("text")
+        .attr("class", "heading")
+        .attr("x", 10)
+        .attr("y", 20)
+        .text(function () {
+            return format(data.value) + " crores"
+        });
+    tooltip.append("text")
+        .attr("class", "description")
+        .attr("x", 10)
+        .attr("y", 50)
+        .text(function () {
+            return data.name
+        });
+}
+
+function hideTooltip(event) {
+    svg.select('.tooltip').remove();
+}
+
+//Load first dataset
+//svg.select('.switcher').classed('selected',true);
+reload(budget2012_2013);
+switchers.filter(function(d) { return d == budget2012_2013 }).classed('selected',true)
